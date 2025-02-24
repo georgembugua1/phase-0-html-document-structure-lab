37dc490fb24c85824e8c2eda0a55df87c3f501c4
georgembugua <georgembugua2@student.moringaschool.com>
2025-02-24 10:57:54 +0300

    document structure lab

---
+++ script.js
@@ -0,0 +1,19 @@
+//what is javascript
+   //one of the most useed programming languages
+   //creative interactive and dynamic elements
+//what can we do with Javascript//
+    //create web/Mpbile apps
+    //Real-time networking apps
+    //Command line tools
+    //Game app Development
+//Where does JavaScript code run
+    //Both the browser and node
+    //Every browser has a JS engine
+       //Chrome-v8
+       //Firefox-Spideermomkey
+ //JavaScript and ECMAScript
+     //is a specification that defines features in Javascript      
+    //ES6
+    console.log("Wesley Asked a Question")
+
+    alert("The weeekend")
